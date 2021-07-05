637c0a19d1be75609c151281a7cffbf434d8f39f
crlotwhite <crlotwhite@gmail.com>
2021-07-06 02:44:13 +0800

    Fix statics 2

--- vsqx2ccs/settings.py
+++ vsqx2ccs/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(os.path.join(BASE_DIR, 'static'), 'img')
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
