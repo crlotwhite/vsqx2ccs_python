e3612a8986b541816eded4af3117fadb57f33ece
crlotwhite <crlotwhite@gmail.com>
2021-07-06 01:50:52 +0800

    Add allow host

--- vsqx2ccs/settings.py
+++ vsqx2ccs/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = get_from_json(json_path, 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['noelvalnet.pythonanywhere.com', ]
 
 
 # Application definition
