d5338e35e99f4ca6d33772feabc636c4fdeb09e7
crlotwhite <crlotwhite@gmail.com>
2021-07-11 22:13:45 +0800

    fix change literal to constant

---
+++ vsfc/constants.py
@@ -0,0 +1,21 @@
+# Ui Translation dict
+ENGLISH_TRANSLATION = {
+    'sub_title': 'File converter for Cevio users',
+    'convert': 'Convert',
+    'just_convert': 'Just Convert',
+    'auto_hiragana': 'Change to Hiragana',
+}
+
+JAPANESE_TRANSLATION = {
+    'sub_title': 'Cevioユーザーのためのファイルコンバータ',
+    'convert': 'ファイルを変換する',
+    'just_convert': '変換するだけ',
+    'auto_hiragana': 'ひらがなに変更',
+}
+
+KOREAN_TRANSLATION = {
+    'sub_title': '체비오 사용자를 위한 파일 변환기',
+    'convert': '변환 하기',
+    'just_convert': '그대로 변환하기',
+    'auto_hiragana': '히라가나로 변환하기',
+}
--- vsfc/views.py
+++ vsfc/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from vsqx2ccs.settings import MEDIA_ROOT
+from .constants import ENGLISH_TRANSLATION, JAPANESE_TRANSLATION, KOREAN_TRANSLATION
 from .forms import UploadFileForm
 
 from ccs.from_vsqx import from_vsqx
@@ -44,32 +45,13 @@ def test(request):
     else:
         language = request.GET.get('lang')
         if language == 'en':
-            translated_strings = {
-                'sub_title': 'File converter for Cevio users',
-                'convert': 'Convert',
-                'just_convert': 'Just Convert',
-                'auto_hiragana': 'Change to Hiragana',
-            }
+            translated_strings = ENGLISH_TRANSLATION
         elif language == 'jp':
-            translated_strings = {
-                'sub_title': 'Cevioユーザーのためのファイルコンバータ',
-                'convert': 'ファイルを変換する',
-                'just_convert': '変換するだけ',
-                'auto_hiragana': 'ひらがなに変更',
-            }
+            translated_strings = JAPANESE_TRANSLATION
         elif language == 'kr':
-            translated_strings = {
-                'sub_title': '체비오 사용자를 위한 파일 변환기',
-                'convert': '변환 하기',
-                'just_convert': '그대로 변환하기',
-                'auto_hiragana': '히라가나로 변환하기',
-            }
+            translated_strings = KOREAN_TRANSLATION
         else:
-            translated_strings = {
-                'sub_title': '체비오 사용자를 위한 파일 변환기',
-                'convert': '변환 하기',
-                'just_convert': '그대로 변환하기',
-                'auto_hiragana': '히라가나로 변환하기',
-            }
+            translated_strings = KOREAN_TRANSLATION
+
         return render(request, 'index.html', translated_strings)
 
